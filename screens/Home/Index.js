80931aa94cd3be89ef87885974eb7f1b3eb08889
Seundede <omadoyedede0@gmail.com>
2023-01-12 09:32:04 +0100

    Revert last commit

--- screens/Home/Index.js
+++ screens/Home/Index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => {
     return (
-      <View className="flex-1 items-center justify-center bg-white">
+      <View>
         <Text>Home</Text>
       </View>
     );
