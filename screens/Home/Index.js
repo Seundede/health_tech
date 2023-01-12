14e58fa439aa94107a7f9842186ef49133aaa962
Seundede <omadoyedede0@gmail.com>
2023-01-12 09:36:56 +0100

    Chore: Set up tailwind

--- screens/Home/Index.js
+++ screens/Home/Index.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 const Home = () => {
     return (
-      <View>
+      <View style={tw`p-4 pt-12 bg-black w-full text-white h-full`}>
         <Text>Home</Text>
       </View>
     );
